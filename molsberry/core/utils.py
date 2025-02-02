7af5b0b3e2edf97fb1ab69eea0fa132dac7ac780
arazthexd <ghanbarpour.araz@gmail.com>
2025-02-02 17:40:24 +0330

    utility changes (random name/path in dir + rdkit)

--- molsberry/core/utils.py
+++ molsberry/core/utils.py
@@ -4,6 +4,20 @@ from contextlib import contextmanager
 def generate_random_str(n: int):
     return "".join(random.choices(string.ascii_uppercase + string.digits, k=n))
 
+def generate_name_in_dir(n: int, dir: str, post: str):
+    while True:
+        s = generate_random_str(n)
+        if not os.path.exists(os.path.join(dir, s+post)):
+            break
+
+    return s
+
+def generate_path_in_dir(n: int, dir: str, post: str):
+    name = generate_name_in_dir(n, dir, post)
+    path = os.path.join(dir, f"{name}{post}")
+
+    return path
+
 @contextmanager
 def suppress_prints():
     original_stdout = sys.stdout
