0f572207dd2c8e14322cdc166903b9ef3ccef457
arazthexd <ghanbarpour.araz@gmail.com>
2025-02-02 17:41:11 +0330

    init change of core

--- molsberry/core/__init__.py
+++ molsberry/core/__init__.py
@@ -1,4 +1,7 @@
 from .data import *
 from .templates import *
 from .pipeline import Pipeline, PipelineBlock, InputBlock, OutputBlock
-from .utils import generate_random_str, suppress_prints
+from .utils import (
+    generate_random_str, suppress_prints, 
+    generate_name_in_dir, generate_path_in_dir
+)
