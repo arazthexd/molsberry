6edc54b7b3629ff7f85b8a402d937e145b3d5476
ArianTajdoost10 <arian@Arian-td>
2025-02-22 22:12:27 +0330

    parmed_mol_rep from pdb_path_rep corrected

--- molsberry/modules/parmed/representations.py
+++ molsberry/modules/parmed/representations.py
@@ -16,6 +16,12 @@ BOND_ORDER_TO_RDBONDTYPE = {
     3.0: Chem.BondType.TRIPLE,
 }
 
+def check_atom_and_res_names(atom1, atom2, name1, name2, resname=None):
+    if resname is not None:
+        if atom1.residue.name != resname or atom2.residue.name != resname:
+            return False
+    return set([atom1.name, atom2.name]) == set([name1, name2])
+
 class ParmedMolRep(MoleculeRep): # TODO: Parameterized vs NonParama
     rep_name = "Parm_mol"
     def __init__(self, structure: parmed.Structure):
@@ -44,18 +50,81 @@ class ParmedMolRep(MoleculeRep): # TODO: Parameterized vs NonParama
     
     @classmethod
     def from_PDBPathRep(cls, pdbrep: PDBPathRep) -> ParmedMolRep:
-        obConversion = openbabel.OBConversion()
-        obConversion.SetInAndOutFormats("pdb", "mol2")
-        mol = openbabel.OBMol()
-        obConversion.ReadFile(mol, pdbrep.content)
-        mol2_file = generate_path_in_dir(3, '.', '_obabel.mol2')
-        obConversion.WriteFile(mol, mol2_file)
-        structure = parmed.load_file(mol2_file, structure=True)
-        os.remove(mol2_file) # TODO: do this with obConversion.WriteString
-        structure: parmed.Structure
-        [setattr(bond, 'order', 1) for bond in structure.bonds if bond.order == 1.5]   # TODO: should we keep this ???
-        return cls(structure)
-        
+
+        st: parmed.Structure = parmed.load_file(pdbrep.content)
+        for bond in st.bonds:
+            bond: parmed.Bond
+            atom1: parmed.Atom = bond.atom1
+            atom2: parmed.Atom = bond.atom2
+            res1: parmed.Residue = atom1.residue
+            res2: parmed.Residue = atom2.residue
+
+            # C=O for all AAs
+            if check_atom_and_res_names(atom1, atom2, "C", "O"):
+                bond.order = 2.0
+            
+            # NH1=CZ for ARG
+            if check_atom_and_res_names(atom1, atom2, "NH1", "CZ", "ARG"):
+                bond.order = 2.0
+
+            # CG=OD1 for ASN
+            if check_atom_and_res_names(atom1, atom2, "CG", "OD1", "ASN"):
+                bond.order = 2.0
+
+            # CG=OD1 for ASP
+            if check_atom_and_res_names(atom1, atom2, "CG", "OD1", "ASP"):
+                bond.order = 2.0
+
+            # CD=OE1 for GLN
+            if check_atom_and_res_names(atom1, atom2, "CD", "OE1", "GLN"):
+                bond.order = 2.0
+
+            # CD=OE1 for GLU
+            if check_atom_and_res_names(atom1, atom2, "CD", "OE1", "GLU"):
+                bond.order = 2.0
+
+            # ND1=CE1 and CD2=CG for HIE, HIS, HIP
+            if res1.name in ["HIE", "HIS", "HIP"]:
+                if check_atom_and_res_names(atom1, atom2, "ND1", "CE1"):
+                    bond.order = 2.0
+                if check_atom_and_res_names(atom1, atom2, "CD2", "CG"):
+                    bond.order = 2.0
+            
+            # NE2=CE1 and CD2=CG for HID (Histidine with delta nitrogen protonated)
+            if check_atom_and_res_names(atom1, atom2, "NE2", "CE1", "HID"):
+                bond.order = 2.0
+            if check_atom_and_res_names(atom1, atom2, "CD2", "CG", "HID"):
+                bond.order = 2.0
+
+            # ND1=CE1 and CD2=CG for HIP (Histidine with both nitrogens protonated)
+            if check_atom_and_res_names(atom1, atom2, "ND1", "CE1", "HIP"):
+                bond.order = 2.0
+            if check_atom_and_res_names(atom1, atom2, "CD2", "CG", "HIP"):
+                bond.order = 2.0
+
+            # Aromatic bonds for PHE (Phenylalanine) and TYR (Tyrosine)
+            if res1.name in ["PHE", "TYR"]:
+                aromatic_bonds = [
+                    ("CG", "CD1"), ("CD1", "CE1"), ("CE1", "CZ"),
+                    ("CZ", "CE2"), ("CE2", "CD2"), ("CD2", "CG")
+                ]
+                for atom_pair in aromatic_bonds:
+                    if check_atom_and_res_names(atom1, atom2, atom_pair[0], atom_pair[1]):
+                        bond.order = 1.5  # Aromatic bond order
+
+            # Aromatic bonds for TRP (Tryptophan) + CG=CD1
+            if res1.name == "TRP":
+                aromatic_bonds = [
+                    ("CD2", "CE2"), ("CE2", "CZ2"), ("CZ2", "CH2"),
+                    ("CH2", "CZ3"), ("CZ3", "CE3"), ("CE3", "CD2")
+                ]
+                for atom_pair in aromatic_bonds:
+                    if check_atom_and_res_names(atom1, atom2, atom_pair[0], atom_pair[1]):
+                        bond.order = 1.5  # Aromatic bond order
+                if check_atom_and_res_names(atom1, atom2, "CG", "CD1"):
+                    bond.order = 2
+        return cls(st)
+            
 
     def to_RDKitMolRep(self) -> RDKitMolRep:
         stmol: parmed.Structure = self.content
